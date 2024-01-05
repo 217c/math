658e3dc6af9685a72add638f65b0776a4e9c0a9d
217c <carlo.fabrizio217@gmail.com>
2024-01-05 19:32:01 +0100

    aggiunta spiegazione in testa

--- 1_pi_is_irrarional/pi_is_irrational.py
+++ 1_pi_is_irrarional/pi_is_irrational.py
@@ -3,6 +3,33 @@ import matplotlib.pyplot as plt
 from matplotlib import collections as mc
 import pylab as pl
 
+"""
+The outer arm spins around at a speed, precisely π times faster than the inner arm. But because π is this never-ending, irrational number, that outer arm will never, ever stop at its starting point, no matter how long the video goes on. It's like a forever chase scene where the arm always misses the target line by the tiniest bit, and that bit is basically as thin as it gets.
+
+Full explanation: https://www.reddit.com/r/oddlysatisfying/comments/17dif1m/comment/k5y7s8i/?share_id=myKxqqS_5fsqgfMwXrXM7&utm_content=2&utm_medium=android_app&utm_name=androidcss&utm_source=share&utm_term=1
+
+For the two ends of the figure to match up perfectly, x has to be a number such that both parts of the formula are eventually back to a previous value (= both rods are at identical rotation angles).
+For example, if x=3, that near miss at 0:12 in the animation would be a hit and the figure would already be complete there.
+But x isn't 3, it's pi = 3.1459... - so the inner rod has already turned a tad further than it should have. So they don't match up.
+You can see that at that moment, the inner rod has done one full rotation and the other one has done just a tad more than 3 full rotations. It's a near miss because that 3/1 = 3 is close to pi.
+[...] The very last close call is at 355/113 (if you're bored, feel free to count the rotations), a famously good approximation for pi - but still just an approximation, not an exact match. If we could ever get to such a fraction for (outer rotation / inner rotations) = pi, the figure would close up perfectly.
+
+Perciò posso calcolare in quale momento andranno a sovrapporsi.
+Ad esempio con 3 andranno a sovrapporsi quando il segmento interno avrà ruotato di 360*1 gradi e quello esterno di 360*3.
+3 infatti sarebbe 3/1 = rotazioni esterne / rotazioni interne. Perciò basta calcolare theta in 360 gradi * 1 giri
+
+con 1/3 ad esempio ci vorrebbero 1 rotazione esterna e 3 rotazioni interne. Questo pone la necessità di calcolare theta in 360 gradi * 3 giri prima che il punto torni all'inizio.
+
+Con qualsiasi numero intero (>1) quindi basta 1 giro.
+Con un numero compreso fra 0 e 1 servono un numero di giri pari al denominatore. Es. 1/3 richiede 3 giri, 1/10 ne richiede 10.
+La forma disegnata è la stessa per un intero e il suo reciproco.
+
+Per qualsiasi frazione serve un numero di giri pari al denominatore.
+
+A questo punto è ovvio che un numero irrazionale non incontrerà mai il punto di partenza, perché non si può individuare il denominatore.
+In python però ovviamente il numero irrazionale non ha infinite cifre decimali, ma avrà una certa approssimazione, e sarà quindi in realtà razionale. Se allora si riesce a trovare il denominatore di quel numero razionale, sarà possibile capire quanti giri ci vogliono affinché il punto torni alla posizione di partenza.
+"""
+
 def set_degrees(from_deg, to_deg, by_step, radians):
     theta_degrees = np.linspace(from_deg,
                                 to_deg,
@@ -76,6 +103,8 @@ def find_min_dist(z, my_coeff, by_step, rounds):
 
     """
     Ho capito che pi ha il suo minimo a 113 giri, quindi è più importante sapere il giro dove c'è il minimo, non lo step.
+    
+    Vedi il commento all'inizio del foglio.
     """
     
     diff = z[1:by_step]-z[0]
@@ -126,7 +155,7 @@ def mixed_coeff(pezzi_dict, theta_radians):
 
 if __name__=='__main__':
     by_step = 10000
-    rounds = 116
+    rounds = 32
     
     theta_radians = set_degrees(from_deg = 0, 
                                 to_deg = rounds*360,
@@ -138,7 +167,8 @@ if __name__=='__main__':
     # numbers_sequence = [0, -3, 5, 2.75, -1/3, 1.61803398875, -np.sqrt(2), 10, -0.75, 7/4, np.pi, -np.e]
     # numbers_sequence = [0, 0.1, 0.2, 0.5, 0.55, 0.555, 0.5555, 0.5555]
     # numbers_sequence = [0]
-    numbers_sequence = [10, 0.3, 1/3, np.sqrt(2), np.pi, np.e]
+    # numbers_sequence = [10, 0.3, 1/3, np.sqrt(2), np.pi, np.e]
+    numbers_sequence = [np.e, np.pi]
     
     for my_coeff in numbers_sequence:
     
